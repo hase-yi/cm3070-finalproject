b57580a6905d0a342dba394067bd5db702660929
hase-yi <83903443+hase-yi@users.noreply.github.com>
2024-06-09 14:43:05 +0000

    Wire up routes for authentification

---
+++ core/user/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import RegisterView, CustomTokenObtainPairView
+
+urlpatterns = [
+  path('register/', RegisterView.as_view(), name = 'register'),
+  path('login/', CustomTokenObtainPairView.as_view(), name='login'),
+]
+
