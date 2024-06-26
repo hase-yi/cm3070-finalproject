97fc2015fdcc6b909d82f7e036492ae9b46f6d2e
hase-yi <83903443+hase-yi@users.noreply.github.com>
2024-06-26 07:32:49 +0000

    Added Pages folder add HomePage.js, defined home route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import Login from './components/Login';
-import Logout from './components/Logout';
-import Home from './components/Home';  // Ensure this matches the file location and export
+import { createBrowserRouter } from 'react-router-dom';
 
-const App = () => {
-    return (
-        <Router>
-            <AuthProvider>
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/logout" element={<Logout />} />
-                    <Route path="/" element={<Home />} />
-                </Routes>
-            </AuthProvider>
-        </Router>
-    );
-};
+import HomePage from './Pages/Home';
+
+createBrowserRouter([
+    {path :'/', element:<HomePage/> },
+    {}
+]);
+
+function App(){
+    return<div></div>;
+}
 
 export default App;
