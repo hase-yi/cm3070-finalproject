511298d1ca67dd2c4d68116b6b6b2966bee66169
hase-yi <83903443+hase-yi@users.noreply.github.com>
2024-06-27 10:03:50 +0000

    edited Error page so it can handle different kinds of errors

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,14 +1,29 @@
 import MainNavigation from '../components/MainNavigation';
+import PageContent from '../components/PageContent';
+import { useRouteError } from 'react-router-dom';
 
 function ErrorPage() {
+	const error = useRouteError();
+
+	let title = 'An error occurred!';
+	let message = 'Something went wrong!';
+
+	if (error.status === 500) {
+		message = error.data.message;
+	}
+
+	if (error.status === 404) {
+		title = 'Not found!';
+		message = 'Could not find resource or page.';
+	}
+
 	return (
-		<>
-			<MainNavigation />
-			<main>
-				<h1>An error occurred</h1>
-				<p>Could not find this page!</p>
-			</main>
-		</>
+    <>
+    <MainNavigation/>
+		<PageContent title={title}>
+			<p>{message}</p>
+		</PageContent>
+    </>
 	);
 }
 
--- src/Pages/Shelves/Shelves.js
+++ src/Pages/Shelves/Shelves.js
@@ -1,12 +1,12 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, json } from 'react-router-dom';
 
 import ShelfList from '../../components/ShelfComponents/ShelfList';
 
 function ShelvesPage() {
 	const shelves = useLoaderData();
 
-	if(shelves.isError){
-		return <p>{shelves.message}</p>
+	if (shelves.isError) {
+		return <p>{shelves.message}</p>;
 	}
 	return <ShelfList shelves={shelves} />;
 }
@@ -16,7 +16,9 @@ export default ShelvesPage;
 export async function loader() {
 	const response = await fetch('http://127.0.0.1:8000/api/shelves/');
 	if (!response.ok) {
-		return {isError:true, message:'Could not fetch shelves'}
+		// return {isError:true, message:'Could not fetch events.'}
+
+		return json({ message: 'Could not fetch shelves' }, { status: 500 });
 	} else {
 		// useLoaderData gives the data that is part of the response
 		return response;
