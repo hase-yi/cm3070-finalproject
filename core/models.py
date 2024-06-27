e444a4af4b1f632550561b42f0fecb9d11f3c483
hase-yi <83903443+hase-yi@users.noreply.github.com>
2024-06-27 08:11:43 +0000

    changed model for shelf, added image

--- core/models.py
+++ core/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Shelf(models.Model):
-    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
+    image = models.URLField(max_length=200, blank=True, null=True) 
 
     def __str__(self):
       return self.name
