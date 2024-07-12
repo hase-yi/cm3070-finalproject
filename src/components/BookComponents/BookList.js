ab2fb64b7010459af6beaa181dc7e6b5331f0057
hase-yi <83903443+hase-yi@users.noreply.github.com>
2024-07-12 08:13:09 +0000

    Add Edit and Delete button to the BookList

--- src/components/BookComponents/BookList.js
+++ src/components/BookComponents/BookList.js
@@ -1,33 +1,37 @@
 import { Link } from 'react-router-dom';
 import classes from './BookList.module.css';
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { fetchBooksForShelf } from '../../features/bookSlice';
+import { fetchBooksForShelf,deleteBook } from '../../features/bookSlice';
 
 const BookList = ({ shelfId }) => {
+	const dispatch = useDispatch();
 
-const dispatch = useDispatch();
+	useEffect(() => {
+		if (shelfId) {
+			dispatch(fetchBooksForShelf(shelfId));
+		}
+	}, [dispatch, shelfId]);
 
-useEffect(()=>{
-	if(shelfId){
-		dispatch(fetchBooksForShelf(shelfId));
+	const books = useSelector((state) => state.books.books);
+	const status = useSelector((state) => state.books.status);
+	const error = useSelector((state) => state.books.error);
+
+
+
+	if (status === 'loading') {
+		return <div>Loading...</div>;
 	}
-},[dispatch, shelfId])
+	if (status === 'failed') {
+		return <div>Error...{error}</div>;
+	}
+	if (!books) {
+		return <div>Books not found</div>;
+	}
 
-const books = useSelector((state)=>state.books.books);
-const status = useSelector((state)=>state.books.status);
-const error = useSelector((state)=>state.books.error);
-
-if(status === 'loading'){
-	return <div>Loading...</div>;
-}
-if(status === 'failed'){
-	return <div>Error...{error}</div>;
-}
-if(!books){
-	return <div>Books not found</div>;
-}
-
+	const handleDelete = (bookId) => {
+		dispatch(deleteBook(bookId));
+	};
 
 	return (
 		<div className={classes.books}>
@@ -40,6 +44,10 @@ if(!books){
 								<h2>{book.title}</h2>
 							</div>
 						</Link>
+						<div className={classes.actions}>
+							<Link to={`/books/${book.id}/edit`} ><button>Edit</button></Link>
+							<button onClick={() => handleDelete(book.id)}>Delete</button>
+						</div>
 					</li>
 				))}
 			</ul>
