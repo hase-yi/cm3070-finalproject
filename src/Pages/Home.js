27f0995f1dd9492357220071657b867a3f3a300e
hase-yi <83903443+hase-yi@users.noreply.github.com>
2024-08-06 06:44:54 +0000

    Adds own books to home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,29 +5,12 @@ import axiosInstance from '../axiosInstance';
 import SharedReviews from '../components/Social/SharedReviews'
 import FollowUnfollowButton from '../components/Social/FollowUnfollowButton'
 import SharedReadingProgress from '../components/Social/SharedReadingProgress'
+import { useDispatch, useSelector } from 'react-redux';
 
 function HomePage() {
   const { username } = useParams();
-  const [reviews, setReviews] = useState([]);
-  const [loadingReviews, setLoadingReviews] = useState(false);
-  const [errorReviews, setErrorreviews] = useState(null);
 
-  useEffect(() => {
-    if (username) {
-      setLoadingReviews(true);
-      setErrorreviews(null);
-
-      axiosInstance.get(`/reviews/`, { params: { username: username } })
-        .then((response) => {
-          setReviews(response.data);
-          setLoadingReviews(false);
-        })
-        .catch((err) => {
-          setErrorreviews('An error occurred while fetching the reviews.');
-          setLoadingReviews(false);
-        });
-    }
-  }, [username]);
+	const user = useSelector((state) => state.auth.user);
 
   return (
     <PageContent title="Welcome">
@@ -37,8 +20,8 @@ function HomePage() {
       {username && <FollowUnfollowButton username={username}/>}
       {/* Show all recent reviews, comments and reading progress that is shared, clicking o them leads to that section in the respective book */}
       
-      {username && <SharedReviews username={username}/>}
-      {username && <SharedReadingProgress username={username}/>}
+      {<SharedReviews username={username ? username : user}/>}
+      {<SharedReadingProgress username={username ? username : user}/>}
     </PageContent>
   )
 }
